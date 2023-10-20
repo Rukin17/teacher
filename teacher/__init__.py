f2b5c0805cd0be01fe09f37f3d4f4176e4f6c329
Andrey Rukin <rukin897@gmail.com>
2023-10-20 21:54:39 +0300

    Первая версия

--- teacher/__init__.py
+++ teacher/__init__.py
@@ -1,6 +1,6 @@
 
-from flask import Flask, render_template, request
-from teacher.model import db, Student, Group
+from flask import Flask, render_template, request, redirect, url_for
+from teacher.model import db, Student, Group, Estimate, Course
 
 
 def create_app():
@@ -14,13 +14,20 @@ def create_app():
         return render_template('menu.html')
     
 
-    @app.route('/class', methods=('POST', 'GET'))
-    def students_in_class():
-        return render_template('class.html', title='Класс')
-    
-
     @app.route('/edit_journal', methods=('POST', 'GET'))
     def edit_journal():
+        all = []
+        try:
+            all_students = Student.query.all()
+            groups = Group.query.all()
+        except:
+            print('Ошибка чтения БД')
+
+        return render_template('edit_journal.html', title='Редактировать журнал', all_students=all_students, groups=groups)
+    
+
+    @app.route('/add_student_and_class', methods=('POST', 'GET'))
+    def add_student_and_class():
         if request.method == 'POST':
             #TODO проверка корректности данных
             try:
@@ -48,7 +55,71 @@ def create_app():
                 db.session.rollback()
                 print('Ошибка добавления в БД')
 
-        return render_template('edit_journal.html', title='Добавить студента')
+        return render_template('add_student_and_class.html', title='Добавить студента')
+    
+
+    @app.route('/delete_student', methods=('POST', 'GET'))
+    def delete_student():
+        if request.method == "POST":
+            id = request.form['delete_student']
+            student = db.get_or_404(Student, id)
+            db.session.delete(student)
+            db.session.commit()
+
+        return render_template("delete_student.html", title='Удалить студента')
+
+    
+    @app.route('/add_course', methods=('POST', 'GET'))
+    def add_course():
+        if request.method == 'POST':
+            course = Course(course_name=request.form['course'])         
+            db.session.add(course)
+            db.session.commit()
+
+        return render_template('add_course.html', title='Добавить предмет') 
+    
+
+    @app.route('/journal', methods=('POST', 'GET'))
+    def journal():
+        groups = Group.query.all()
+        if request.method == 'POST':
+            group = request.form['group_name']
+            if group:
+                return redirect(f'/journal/{group}', code=302)
+        return render_template('journal.html', title='Журнал', groups=groups)
+
+
+    @app.route('/journal/<group>', methods=('POST', 'GET'))
+    def get_group(group):
+        group_in_database = Group.query.filter(Group.group_name == group).first()
+        students = Student.query.filter(Student.group_id==group_in_database.id).all()
+        courses = Course.query.all()
+        return render_template('groups.html', group=group, students=students, courses=courses)   
+
+
+    @app.route('/journal/<group>/<course>', methods=('POST', 'GET'))
+    def get_estimates_table(group, course):
+        group_in_database = Group.query.filter(Group.group_name == group).first()
+        students = Student.query.filter(Student.group_id == group_in_database.id).all()
+        course_in_database = Course.query.filter(Course.course_name == course).first()
+        estimates = Estimate.query.filter(Estimate.course_id == course_in_database.id).all()
+
+        return render_template('estimates.html', students=students, course=course, group=group, estimates=estimates)
+
+
+    @app.route('/journal/<group>/<course>/add_estimate', methods=('POST', 'GET'))
+    def add_estimate(group, course):
+        group_in_database = Group.query.filter(Group.group_name == group).first()
+        students = Student.query.filter(Student.group_id == group_in_database.id).all()
+        if request.method == 'POST':
+            course_in_database = Course.query.filter(Course.course_name == course).first()
+            new_estimate = Estimate(estimate=request.form['estimate'], student_id=request.form['student_id'], course_id=course_in_database.id)
+            db.session.add(new_estimate)
+            db.session.commit()
+            return  redirect(url_for('get_estimates_table', group=group, course=course))
+        return render_template('add_estimate.html', students=students)
     
 
     return app
+
+   
--- teacher/model.py
+++ teacher/model.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 
 class Group(db.Model):
-    __tablename__ = 'group'
+    __tablename__ = 'groups'
     
     id = db.Column(db.Integer, primary_key=True)
     group_name = db.Column(db.String(50), nullable = True)
@@ -15,13 +15,38 @@ class Group(db.Model):
     
 
 class Student(db.Model):
-    __tablename__ = 'student'
+    __tablename__ = 'students'
 
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(50), nullable = True)
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
     
-    group = relationship("Group", backref="student")
+    group = relationship("Group", backref="students")
 
     def __repr__(self):
         return f'<student {self.id}>' 
+    
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.String(50), nullable = True)
+
+    def __repr__(self):
+        return f'<course {self.id}>'
+
+
+class Estimate(db.Model):
+    __tablename__ = 'estimates'
+
+    id = db.Column(db.Integer, primary_key=True)
+    estimate = db.Column(db.String(50), nullable=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'))
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'))
+
+    student = relationship('Student', backref='estimates')
+    course = relationship('Course', backref='estimates')
+
+    def __repr__(self):
+        return f'<estimate {self.id}>'
